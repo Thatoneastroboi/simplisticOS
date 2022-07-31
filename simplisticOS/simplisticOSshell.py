de1e5d3d6f01fcffa3c8b8e83e0be2cf62ec1d6c
Thatoneastroboi <110192618+Thatoneastroboi@users.noreply.github.com>
2022-07-30 20:02:45 -0700

    Add files via upload

--- simplisticOS/simplisticOSshell.py
+++ simplisticOS/simplisticOSshell.py
@@ -1,6 +1,10 @@
 import math
 import tkinter
 root = tkinter.Tk()
-label = tkinter.Label(root, text="welcome to simplistic OS!")
+label = tkinter.Label(root, text="welcome to simplistic OS! please enter a password for your account")
+passwordInput = tkinter.Entry(root)
+submitButton = tkinter.Button(root, text="submit")
 label.pack()
+passwordInput.pack()
+submitButton.pack
 root.mainloop()
